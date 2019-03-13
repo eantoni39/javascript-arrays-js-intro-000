9712d1960699d40d7a922d58203cc2e4b0320a35
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-13 02:41:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,22 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 function addElementToBeginningOfArray (array,element) {
-  array.unshift(element)
-  return array
+  var nouveau = [element,...array]
+  return nouveau
 }
 
 function destructivelyAddElementToBeginningOfArray (array,element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray (array,element) {
+  var nouveau = [...array,element]
+  return nouveau
+}
+
+function destructivelyAddElementToEndOfArray (array,element) {
+  array.push(element)
+  return array
+}
+
+
